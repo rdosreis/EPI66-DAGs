728fa88f67974678970459185128d34e07cc2149
rdosreis <rodrigocpdosreis@gmail.com>
2023-06-20 06:40:41 -0300

    Atividade 1

---
+++ RScripts/01_ATI_EPI66-DAGs.R
@@ -0,0 +1,5 @@
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='90%'-------------------
+
+knitr::include_graphics(here('imagens', 'caffeine-causality-loop.jpg'))
+
+
--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,8 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "02_EPI66-DAGs"
+pasta <- "Rmds_atividades"
+arquivo <- "01_ATI_EPI66-DAGs"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
